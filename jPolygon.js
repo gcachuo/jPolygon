390ff4827f7c8e3f73cf0d1846d4bb391d88f236
Memo Cachú <gcachu.o@gmail.com>
2020-06-09 21:43:40 -0500

    jquery wrapper

--- jPolygon.js
+++ jPolygon.js
@@ -6,165 +6,182 @@
    License: MIT License
 */
 
-var perimeter = new Array();
-var complete = false;
-var canvas = document.getElementById("jPolygon");
-var ctx;
-
-function line_intersects(p0, p1, p2, p3) {
-    var s1_x, s1_y, s2_x, s2_y;
-    s1_x = p1['x'] - p0['x'];
-    s1_y = p1['y'] - p0['y'];
-    s2_x = p3['x'] - p2['x'];
-    s2_y = p3['y'] - p2['y'];
-
-    var s, t;
-    s = (-s1_y * (p0['x'] - p2['x']) + s1_x * (p0['y'] - p2['y'])) / (-s2_x * s1_y + s1_x * s2_y);
-    t = (s2_x * (p0['y'] - p2['y']) - s2_y * (p0['x'] - p2['x'])) / (-s2_x * s1_y + s1_x * s2_y);
-
-    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
-        // Collision detected
-        return true;
-    }
-    return false; // No collision
-}
-
-function point(x, y) {
-    ctx.fillStyle = "white";
-    ctx.strokeStyle = "white";
-    ctx.fillRect(x - 2, y - 2, 4, 4);
-    ctx.moveTo(x, y);
-}
-
-function undo() {
-    ctx = undefined;
-    perimeter.pop();
+class jPolygon {
+    perimeter = [];
     complete = false;
-    start(true);
-}
+    canvas;
+    ctx;
 
-function clear_canvas() {
-    ctx = undefined;
-    perimeter = new Array();
-    complete = false;
-    document.getElementById('coordinates').value = '';
-    start();
-}
-
-function draw(end) {
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "white";
-    ctx.lineCap = "square";
-    ctx.beginPath();
-
-    for (var i = 0; i < perimeter.length; i++) {
-        if (i == 0) {
-            ctx.moveTo(perimeter[i]['x'], perimeter[i]['y']);
-            end || point(perimeter[i]['x'], perimeter[i]['y']);
-        } else {
-            ctx.lineTo(perimeter[i]['x'], perimeter[i]['y']);
-            end || point(perimeter[i]['x'], perimeter[i]['y']);
-        }
+    constructor(_canvas, _settings) {
+        this.canvas = _canvas;
+        this.clear_canvas();
     }
-    if (end) {
-        ctx.lineTo(perimeter[0]['x'], perimeter[0]['y']);
-        ctx.closePath();
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
-        ctx.fill();
-        ctx.strokeStyle = 'blue';
-        complete = true;
-    }
-    ctx.stroke();
 
-    // print coordinates
-    if (perimeter.length == 0) {
+    line_intersects(p0, p1, p2, p3) {
+        let s1_x, s1_y, s2_x, s2_y;
+        s1_x = p1['x'] - p0['x'];
+        s1_y = p1['y'] - p0['y'];
+        s2_x = p3['x'] - p2['x'];
+        s2_y = p3['y'] - p2['y'];
+
+        let s, t;
+        s = (-s1_y * (p0['x'] - p2['x']) + s1_x * (p0['y'] - p2['y'])) / (-s2_x * s1_y + s1_x * s2_y);
+        t = (s2_x * (p0['y'] - p2['y']) - s2_y * (p0['x'] - p2['x'])) / (-s2_x * s1_y + s1_x * s2_y);
+
+        return s >= 0 && s <= 1 && t >= 0 && t <= 1;
+    }
+
+    point(x, y) {
+        this.ctx.fillStyle = "white";
+        this.ctx.strokeStyle = "white";
+        this.ctx.fillRect(x - 2, y - 2, 4, 4);
+        this.ctx.moveTo(x, y);
+    }
+
+    undo() {
+        this.ctx = undefined;
+        this.perimeter.pop();
+        this.complete = false;
+        this.start(true);
+    }
+
+    clear_canvas() {
+        this.ctx = undefined;
+        this.perimeter = [];
+        this.complete = false;
         document.getElementById('coordinates').value = '';
-    } else {
-        document.getElementById('coordinates').value = JSON.stringify(perimeter);
+        this.start();
+    }
+
+    draw(end) {
+        this.ctx.lineWidth = 1;
+        this.ctx.strokeStyle = "white";
+        this.ctx.lineCap = "square";
+        this.ctx.beginPath();
+
+        for (let i = 0; i < this.perimeter.length; i++) {
+            if (i === 0) {
+                this.ctx.moveTo(this.perimeter[i]['x'], this.perimeter[i]['y']);
+                end || this.point(this.perimeter[i]['x'], this.perimeter[i]['y']);
+            } else {
+                this.ctx.lineTo(this.perimeter[i]['x'], this.perimeter[i]['y']);
+                end || this.point(this.perimeter[i]['x'], this.perimeter[i]['y']);
+            }
+        }
+        if (end) {
+            this.ctx.lineTo(this.perimeter[0]['x'], this.perimeter[0]['y']);
+            this.ctx.closePath();
+            this.ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+            this.ctx.fill();
+            this.ctx.strokeStyle = 'blue';
+            this.complete = true;
+        }
+        this.ctx.stroke();
+
+        // print coordinates
+        if (this.perimeter.length === 0) {
+            document.getElementById('coordinates').value = '';
+        } else {
+            document.getElementById('coordinates').value = JSON.stringify(this.perimeter);
+        }
+    }
+
+    check_intersect(x, y) {
+        if (this.perimeter.length < 4) {
+            return false;
+        }
+        const p0 = [];
+        const p1 = [];
+        const p2 = [];
+        const p3 = [];
+
+        p2['x'] = this.perimeter[this.perimeter.length - 1]['x'];
+        p2['y'] = this.perimeter[this.perimeter.length - 1]['y'];
+        p3['x'] = x;
+        p3['y'] = y;
+
+        for (let i = 0; i < this.perimeter.length - 1; i++) {
+            p0['x'] = this.perimeter[i]['x'];
+            p0['y'] = this.perimeter[i]['y'];
+            p1['x'] = this.perimeter[i + 1]['x'];
+            p1['y'] = this.perimeter[i + 1]['y'];
+            if (p1['x'] === p2['x'] && p1['y'] === p2['y']) {
+                continue;
+            }
+            if (p0['x'] === p3['x'] && p0['y'] === p3['y']) {
+                continue;
+            }
+            if (this.line_intersects(p0, p1, p2, p3) === true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    point_it(event) {
+        if (this.complete) {
+            alert('Polygon already created');
+            return false;
+        }
+        let rect, x, y;
+
+        if (event.ctrlKey || event.which === 3 || event.button === 2) {
+            if (this.perimeter.length === 2) {
+                alert('You need at least three points for a polygon');
+                return false;
+            }
+            x = this.perimeter[0]['x'];
+            y = this.perimeter[0]['y'];
+            if (this.check_intersect(x, y)) {
+                alert('The line you are drowing intersect another line');
+                return false;
+            }
+            this.draw(true);
+            alert('Polygon closed');
+            event.preventDefault();
+            return false;
+        } else {
+            rect = this.canvas.getBoundingClientRect();
+            x = event.clientX - rect.left;
+            y = event.clientY - rect.top;
+            if (this.perimeter.length > 0 && x === this.perimeter[this.perimeter.length - 1]['x'] && y === this.perimeter[this.perimeter.length - 1]['y']) {
+                // same point - double click
+                return false;
+            }
+            if (this.check_intersect(x, y)) {
+                alert('The line you are drowing intersect another line');
+                return false;
+            }
+            this.perimeter.push({'x': x, 'y': y});
+            this.draw(false);
+            return false;
+        }
+    }
+
+    start(with_draw) {
+        const img = new Image();
+        img.src = this.canvas.getAttribute('data-imgsrc');
+
+        img.onload = () => {
+            this.ctx = this.canvas.getContext("2d");
+            this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+            if (with_draw === true) {
+                this.draw(false);
+            }
+        }
     }
 }
 
-function check_intersect(x, y) {
-    if (perimeter.length < 4) {
-        return false;
+$.fn.extend({
+    jPolygon: function (settings) {
+        const $this = this;
+        const jpolygon = new jPolygon($this.get(0), settings);
+        $this
+            .on('mousedown', (event) => {
+                jpolygon.point_it(event);
+            })
+            .on('contextmenu', (event) => {
+                return false;
+            });
     }
-    var p0 = new Array();
-    var p1 = new Array();
-    var p2 = new Array();
-    var p3 = new Array();
-
-    p2['x'] = perimeter[perimeter.length - 1]['x'];
-    p2['y'] = perimeter[perimeter.length - 1]['y'];
-    p3['x'] = x;
-    p3['y'] = y;
-
-    for (var i = 0; i < perimeter.length - 1; i++) {
-        p0['x'] = perimeter[i]['x'];
-        p0['y'] = perimeter[i]['y'];
-        p1['x'] = perimeter[i + 1]['x'];
-        p1['y'] = perimeter[i + 1]['y'];
-        if (p1['x'] == p2['x'] && p1['y'] == p2['y']) {
-            continue;
-        }
-        if (p0['x'] == p3['x'] && p0['y'] == p3['y']) {
-            continue;
-        }
-        if (line_intersects(p0, p1, p2, p3) == true) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function point_it(event) {
-    if (complete) {
-        alert('Polygon already created');
-        return false;
-    }
-    var rect, x, y;
-
-    if (event.ctrlKey || event.which === 3 || event.button === 2) {
-        if (perimeter.length == 2) {
-            alert('You need at least three points for a polygon');
-            return false;
-        }
-        x = perimeter[0]['x'];
-        y = perimeter[0]['y'];
-        if (check_intersect(x, y)) {
-            alert('The line you are drowing intersect another line');
-            return false;
-        }
-        draw(true);
-        alert('Polygon closed');
-        event.preventDefault();
-        return false;
-    } else {
-        rect = canvas.getBoundingClientRect();
-        x = event.clientX - rect.left;
-        y = event.clientY - rect.top;
-        if (perimeter.length > 0 && x == perimeter[perimeter.length - 1]['x'] && y == perimeter[perimeter.length - 1]['y']) {
-            // same point - double click
-            return false;
-        }
-        if (check_intersect(x, y)) {
-            alert('The line you are drowing intersect another line');
-            return false;
-        }
-        perimeter.push({'x': x, 'y': y});
-        draw(false);
-        return false;
-    }
-}
-
-function start(with_draw) {
-    var img = new Image();
-    img.src = canvas.getAttribute('data-imgsrc');
-
-    img.onload = function () {
-        ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        if (with_draw == true) {
-            draw(false);
-        }
-    }
-}
+});
