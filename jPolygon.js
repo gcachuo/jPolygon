3173db19840fe47588626c5ae2a2a6951deccc05
Memo Cachú <gcachu.o@gmail.com>
2020-06-09 21:23:14 -0500

    cleanup

--- jPolygon.js
+++ jPolygon.js
@@ -20,31 +20,30 @@ function line_intersects(p0, p1, p2, p3) {
 
     var s, t;
     s = (-s1_y * (p0['x'] - p2['x']) + s1_x * (p0['y'] - p2['y'])) / (-s2_x * s1_y + s1_x * s2_y);
-    t = ( s2_x * (p0['y'] - p2['y']) - s2_y * (p0['x'] - p2['x'])) / (-s2_x * s1_y + s1_x * s2_y);
+    t = (s2_x * (p0['y'] - p2['y']) - s2_y * (p0['x'] - p2['x'])) / (-s2_x * s1_y + s1_x * s2_y);
 
-    if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
-    {
+    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
         // Collision detected
         return true;
     }
     return false; // No collision
 }
 
-function point(x, y){
-    ctx.fillStyle="white";
+function point(x, y) {
+    ctx.fillStyle = "white";
     ctx.strokeStyle = "white";
-    ctx.fillRect(x-2,y-2,4,4);
-    ctx.moveTo(x,y);
+    ctx.fillRect(x - 2, y - 2, 4, 4);
+    ctx.moveTo(x, y);
 }
 
-function undo(){
+function undo() {
     ctx = undefined;
     perimeter.pop();
     complete = false;
     start(true);
 }
 
-function clear_canvas(){
+function clear_canvas() {
     ctx = undefined;
     perimeter = new Array();
     complete = false;
@@ -52,23 +51,23 @@ function clear_canvas(){
     start();
 }
 
-function draw(end){
+function draw(end) {
     ctx.lineWidth = 1;
     ctx.strokeStyle = "white";
     ctx.lineCap = "square";
     ctx.beginPath();
 
-    for(var i=0; i<perimeter.length; i++){
-        if(i==0){
-            ctx.moveTo(perimeter[i]['x'],perimeter[i]['y']);
-            end || point(perimeter[i]['x'],perimeter[i]['y']);
+    for (var i = 0; i < perimeter.length; i++) {
+        if (i == 0) {
+            ctx.moveTo(perimeter[i]['x'], perimeter[i]['y']);
+            end || point(perimeter[i]['x'], perimeter[i]['y']);
         } else {
-            ctx.lineTo(perimeter[i]['x'],perimeter[i]['y']);
-            end || point(perimeter[i]['x'],perimeter[i]['y']);
+            ctx.lineTo(perimeter[i]['x'], perimeter[i]['y']);
+            end || point(perimeter[i]['x'], perimeter[i]['y']);
         }
     }
-    if(end){
-        ctx.lineTo(perimeter[0]['x'],perimeter[0]['y']);
+    if (end) {
+        ctx.lineTo(perimeter[0]['x'], perimeter[0]['y']);
         ctx.closePath();
         ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
         ctx.fill();
@@ -78,15 +77,15 @@ function draw(end){
     ctx.stroke();
 
     // print coordinates
-    if(perimeter.length == 0){
+    if (perimeter.length == 0) {
         document.getElementById('coordinates').value = '';
     } else {
         document.getElementById('coordinates').value = JSON.stringify(perimeter);
     }
 }
 
-function check_intersect(x,y){
-    if(perimeter.length < 4){
+function check_intersect(x, y) {
+    if (perimeter.length < 4) {
         return false;
     }
     var p0 = new Array();
@@ -94,19 +93,23 @@ function check_intersect(x,y){
     var p2 = new Array();
     var p3 = new Array();
 
-    p2['x'] = perimeter[perimeter.length-1]['x'];
-    p2['y'] = perimeter[perimeter.length-1]['y'];
+    p2['x'] = perimeter[perimeter.length - 1]['x'];
+    p2['y'] = perimeter[perimeter.length - 1]['y'];
     p3['x'] = x;
     p3['y'] = y;
 
-    for(var i=0; i<perimeter.length-1; i++){
+    for (var i = 0; i < perimeter.length - 1; i++) {
         p0['x'] = perimeter[i]['x'];
         p0['y'] = perimeter[i]['y'];
-        p1['x'] = perimeter[i+1]['x'];
-        p1['y'] = perimeter[i+1]['y'];
-        if(p1['x'] == p2['x'] && p1['y'] == p2['y']){ continue; }
-        if(p0['x'] == p3['x'] && p0['y'] == p3['y']){ continue; }
-        if(line_intersects(p0,p1,p2,p3)==true){
+        p1['x'] = perimeter[i + 1]['x'];
+        p1['y'] = perimeter[i + 1]['y'];
+        if (p1['x'] == p2['x'] && p1['y'] == p2['y']) {
+            continue;
+        }
+        if (p0['x'] == p3['x'] && p0['y'] == p3['y']) {
+            continue;
+        }
+        if (line_intersects(p0, p1, p2, p3) == true) {
             return true;
         }
     }
@@ -114,40 +117,40 @@ function check_intersect(x,y){
 }
 
 function point_it(event) {
-    if(complete){
+    if (complete) {
         alert('Polygon already created');
         return false;
     }
     var rect, x, y;
 
-    if(event.ctrlKey || event.which === 3 || event.button === 2){
-        if(perimeter.length==2){
+    if (event.ctrlKey || event.which === 3 || event.button === 2) {
+        if (perimeter.length == 2) {
             alert('You need at least three points for a polygon');
             return false;
         }
         x = perimeter[0]['x'];
         y = perimeter[0]['y'];
-        if(check_intersect(x,y)){
+        if (check_intersect(x, y)) {
             alert('The line you are drowing intersect another line');
             return false;
         }
         draw(true);
         alert('Polygon closed');
-	event.preventDefault();
+        event.preventDefault();
         return false;
     } else {
         rect = canvas.getBoundingClientRect();
         x = event.clientX - rect.left;
         y = event.clientY - rect.top;
-        if (perimeter.length>0 && x == perimeter[perimeter.length-1]['x'] && y == perimeter[perimeter.length-1]['y']){
+        if (perimeter.length > 0 && x == perimeter[perimeter.length - 1]['x'] && y == perimeter[perimeter.length - 1]['y']) {
             // same point - double click
             return false;
         }
-        if(check_intersect(x,y)){
+        if (check_intersect(x, y)) {
             alert('The line you are drowing intersect another line');
             return false;
         }
-        perimeter.push({'x':x,'y':y});
+        perimeter.push({'x': x, 'y': y});
         draw(false);
         return false;
     }
@@ -157,10 +160,10 @@ function start(with_draw) {
     var img = new Image();
     img.src = canvas.getAttribute('data-imgsrc');
 
-    img.onload = function(){
+    img.onload = function () {
         ctx = canvas.getContext("2d");
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        if(with_draw == true){
+        if (with_draw == true) {
             draw(false);
         }
     }
